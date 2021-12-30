483b0b76db961a09c727c61a7e04895e3a897751
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-12-30 13:22:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 peter_pan_song="";
 harry_potter_song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
 
 function preload(){
     peter_pan_song=loadSound("music2.mp3");
@@ -11,8 +16,30 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized.');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("LeftWristX = "+leftWristX+", LeftWristY = "+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("RightWristX = "+rightWristX+", RightWristY = "+rightWristY);
+    }
 }
 
 function draw(){
     image(video,0,0,600,500);
 }
+
